55357fe693735ac2584086f50fd773f5d4ee0d42
windows <edgar.walugembe.upti@gmail.com>
2023-12-06 09:40:48 +0300

    backend && frontend 0007

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../database/models");
 
-router.post("/", function (req, res, next) {
-  return res.status(200).json({ message: "registered successfully" });
+router.post("/", async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  const alreadyExitsUser = await User.findOne({
+    where: { username, email },
+  }).catch((err) => {
+    console.log("error: ", err);
+  });
+  if (alreadyExitsUser) {
+    res.json({ message: "User with username or email already exists!" });
+  }
+
+  const newUser = new User({ username, email, password });
+  const savedUser = await newUser.save().catch((err) => {
+    console.log("error saving user: ", err);
+    res.json({ error: "Cannot register User right away!" });
+  });
+
+  if (savedUser) {
+    res.status(200).json({ message: "registered successfully" });
+  }
+
+  console.log(req.body);
 });
 
 module.exports = router;
+
+//29:20
